fa5d461d31837380db52e1bc87e46479241ee442
dk58sh32 <86909337+dk58sh32@users.noreply.github.com>
2022-04-16 17:41:19 +0800

    dxj

---
+++ home.js
@@ -0,0 +1,23 @@
+(function(w) {
+    function enterLine() {
+        document.getElementById("btnEnter1").onclick = function() {
+            window.open('http://w5.w5r465e45fd.online:11295', "_blank");
+        }
+        document.getElementById("btnEnter2").onclick = function() {
+            window.open('http://4r.4r4wet5r64t.club:11295', "_blank");
+        }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('http://kd.kdjfiu8w7rt.co:11295', "_blank");
+        }
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('http://mf.mfcmfdjsc.info:11295', "_blank");
+        }
+        document.getElementById("btnEnter5").onclick = function() {
+            window.open('http://7j.7jfdksjkjf.xyz:11295', "_blank");
+        }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);
