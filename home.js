7c573b82be545d15bee4b0df6f510bbf3b217544
dxjbar <103813576+dxjbar@users.noreply.github.com>
2022-05-18 18:37:03 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('http://23.231d3f1g32f13h.cc', "_blank");
+            window.open('https://4h.4hg56j4654jk65.info', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('http://21.21g321j3h4k65f.cc', "_blank");
+            window.open('https://h4.h4g65ty4654567.co', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('http://46.465h432cv1b23x.xyz', "_blank");
+            window.open('https://56.56456546346545.co', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('http://c1.c1b32x1ncfg56d.info', "_blank");
+            window.open('https://4h.4hg56j4654jk65.info', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('http://13.13cv21x32c1b2v.xyz', "_blank");
+            window.open('https://h4.h4g65ty4654567.co', "_blank");
         }
     }
     function init() {
