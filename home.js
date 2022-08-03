4d3ee4330ef66a6879221967b9c988891f6fd413
dxjbar <103813576+dxjbar@users.noreply.github.com>
2022-08-03 15:01:33 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://api.american1b.cc', "_blank");
+            window.open('https://api.application8u.cc', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://api.american8j.cc', "_blank");
+            window.open('https://api.approach2n.cc', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://api.american1b.cc', "_blank");
+            window.open('https://api.application8u.cc', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://api.american8j.cc', "_blank");
+            window.open('https://api.approach2n.cc', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('https://api.american1b.cc', "_blank");
+            window.open('https://api.application8u.cc', "_blank");
         }
     }
     function init() {
