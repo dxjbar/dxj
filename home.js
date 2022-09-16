0d78a6eb1b6e180ae86c2245aedb75c6cc3c13ab
dxjbar <103813576+dxjbar@users.noreply.github.com>
2022-09-16 14:27:14 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://api.bkvqd5ts.life', "_blank");
+            window.open('https://api.r0632cg4.world', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://api.vat7shfg.world', "_blank");
+            window.open('https://api.b49darxf.cc', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://api.bkvqd5ts.life', "_blank");
+            window.open('https://api.r0632cg4.world', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://api.vat7shfg.world', "_blank");
+            window.open('https://api.b49darxf.cc', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('https://api.bkvqd5ts.life', "_blank");
+            window.open('https://api.r0632cg4.world', "_blank");
         }
     }
     function init() {
