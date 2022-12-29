19d698b697a6c2dcf86776b5860ad238df507d32
dxjbar <103813576+dxjbar@users.noreply.github.com>
2022-12-29 16:21:50 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://api.johsfanc.club', "_blank");
+            window.open('https://api.spehnun9.world', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://api.xuoysrt1.club', "_blank");
+            window.open('https://api.b79tufla.club', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://api.johsfanc.club', "_blank");
+            window.open('https://api.spehnun9.world', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://api.xuoysrt1.club', "_blank");
+            window.open('https://api.b79tufla.club', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('https://api.johsfanc.club', "_blank");
+            window.open('https://api.spehnun9.world', "_blank");
         }
     }
     function init() {
